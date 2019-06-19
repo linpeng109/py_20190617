606ebf75041571ca67e1759cd0c09dcace17873e
linpeng109 <linpeng109@aliyun.com>
2019-06-19 17:40:35 +0800

    20190619

--- subwin.py
+++ subwin.py
@@ -3,8 +3,10 @@ import sys
 import threading
 import time
 
+import win32con
 import win32gui
 from PySide2.QtCore import QProcess
+from PySide2.QtCore import Qt
 from PySide2.QtGui import QWindow
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtWidgets import QMdiArea
@@ -38,14 +40,23 @@ class Example(QMainWindow):
         self.setWindowTitle("测试")
 
         self.calc_hnd = win32gui.FindWindow(None, u"计算器")
+        win32gui.SetWindowLong(self.calc_hnd, win32con.GWL_STYLE, win32con.GW_CHILD | win32con.WS_VISIBLE)
+
         self.calc_win = QWindow.fromWinId(self.calc_hnd)
-        self.calc_win.setGeometry(0, 0, 640, 480)
+        # self.calc_win.setWindowFlags(self.calc_win.flags() | Qt.FramelessWindowHint | Qt.WindowTitleHint)
+        # self.calc_win.setGeometry(0, 0, 640, 480)
         self.calc_wdgt = self.createWindowContainer(self.calc_win)
+        self.calc_wdgt.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setCentralWidget(self.calc_wdgt)
+
         # 设置窗口的父子关系
+        # self.calc_win.setParent(self.winId())
+        # win32gui.SetParent(self.calc_hnd, self)
+        # win32gui.SetParent(self.calc_wdgt, self.widget)
         self.calc_wdgt.setParent(self)
-        # self.showMaximized()
         self.setGeometry(0, 0, 640, 480)
+        # self.show()
+
         self.show()
 
     @staticmethod
